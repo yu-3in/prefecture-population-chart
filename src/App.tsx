ffb0e5cddcda667c478f9b083b6780b13823e3bf
yuuumiravy <hikoki.y@gmail.com>
2023-06-10 19:55:20 +0900

    feat: 疎通確認をテストした

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
-const App = () => <>Hello World</>
+import { useEffect } from 'react'
+import client from './libs/client'
+
+const App = () => {
+  useEffect(() => {
+    ;(async () => {
+      const prefectures = await client.get('prefectures')
+      console.log(prefectures)
+    })()
+  }, [])
+  return <>Hello World</>
+}
 
 export default App
