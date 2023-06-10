64d250c6109b4ff51a2ac4785bd33d5a9fda9937
yuuumiravy <hikoki.y@gmail.com>
2023-06-11 01:52:33 +0900

    refactor: " -> ' at .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,18 +16,18 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    "react/react-in-jsx-scope": "off",
-    "react/function-component-definition": [
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [
       2,
       {
-        namedComponents: "arrow-function",
-        unnamedComponents: "arrow-function",
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
       },
     ],
   },
   ignorePatterns: [
-    ".eslintrc.cjs",
-    "vite.config.ts",
-    "api/*"
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    'api/*'
   ],
 };
