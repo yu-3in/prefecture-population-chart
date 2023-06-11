9118bbdf76e2f7b96ac4109a4fd6107acbd63048
yuuumiravy <hikoki.y@gmail.com>
2023-06-11 17:35:54 +0900

    feat: 背景色を薄いグレーにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { Provider } from 'react-redux'
 import { store } from './stores/store'
 import { theme } from './theme'
-import { ThemeProvider } from '@emotion/react'
+import { ThemeProvider, css } from '@emotion/react'
 import { PrefectureSelector } from './features/prefectures/components/PrefectureSelector'
 import { PopulationLineChart } from './features/populations/components/PopulationLineChart'
+import { Container } from './components/layouts/Container'
 
 const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <PrefectureSelector />
-        <PopulationLineChart />
+        <main css={main}>
+          <Container>
+            <PrefectureSelector />
+            <PopulationLineChart />
+          </Container>
+        </main>
       </ThemeProvider>
     </Provider>
   )
 }
+
+const main = css`
+  background-color: ${theme.colors.background};
+  min-height: 100vh;
+`
 
 export default App
