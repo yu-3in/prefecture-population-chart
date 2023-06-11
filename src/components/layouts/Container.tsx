fe2ca06c865f1cfeb02a37eb5b7f0581839b9f59
yuuumiravy <hikoki.y@gmail.com>
2023-06-11 17:36:30 +0900

    feat: Containerコンポーネントを作成

---
+++ src/components/layouts/Container.tsx
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react'
+
+export type ContainerProps = {
+  children: React.ReactNode
+}
+
+export const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div css={container}>{children}</div>
+}
+
+const container = css`
+  width: 90%;
+  max-width: 800px;
+  margin: 0 auto;
+`
