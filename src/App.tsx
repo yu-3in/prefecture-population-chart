dd397e256b55a638c9be62af5fae2b6723a48dfe
yuuumiravy <hikoki.y@gmail.com>
2023-06-11 12:13:54 +0900

    feat: 都道府県を選択するコンポーネントを表示する

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { Provider } from 'react-redux'
 import { store } from './stores/store'
 import { theme } from './theme'
 import { ThemeProvider } from '@emotion/react'
+import { PrefectureSelector } from './features/prefectures/components/PrefectureSelector'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>Hello world</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <PrefectureSelector />
+      </ThemeProvider>
     </Provider>
   )
 }
