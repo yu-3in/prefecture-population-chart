f4d33de813a28a44dc275d112236f8efc51ba7a5
yuuumiravy <hikoki.y@gmail.com>
2023-06-11 18:14:09 +0900

    feat: Cardを使って表示するようにした

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider, css } from '@emotion/react'
 import { PrefectureSelector } from './features/prefectures/components/PrefectureSelector'
 import { PopulationLineChart } from './features/populations/components/PopulationLineChart'
 import { Container } from './components/layouts/Container'
+import { Card } from './components/card'
 
 const App = () => {
   return (
@@ -12,8 +13,17 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <main css={main}>
           <Container>
-            <PrefectureSelector />
-            <PopulationLineChart />
+            <div css={content}>
+              <Card>
+                <Card.Head>都道府県を選択してください</Card.Head>
+                <Card.Content>
+                  <PrefectureSelector />
+                </Card.Content>
+              </Card>
+              <Card>
+                <PopulationLineChart />
+              </Card>
+            </div>
           </Container>
         </main>
       </ThemeProvider>
@@ -24,6 +34,13 @@ const App = () => {
 const main = css`
   background-color: ${theme.colors.background};
   min-height: 100vh;
+  padding: 2rem 0;
+`
+
+const content = css`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 `
 
 export default App
