a859f4a1a9ddb5622ffe38876c1290bd1bb77a37
yuuumiravy <hikoki.y@gmail.com>
2023-06-16 01:55:32 +0900

    feat: 選択された人口カテゴリの情報を保持するグローバルステートを作成

---
+++ src/features/populations/stores/SelectedPopulationCategory.ts
@@ -0,0 +1,19 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { PopulationLabel } from '../types/Population'
+
+const initialState: { selected: PopulationLabel } = { selected: '総人口' }
+
+const selectedPopulationCategorySlice = createSlice({
+  name: 'populationCategories',
+  initialState,
+  reducers: {
+    selectPopulationCategory: (state, action: PayloadAction<PopulationLabel>) => {
+      state.selected = action.payload
+      console.log(action.payload)
+    },
+  },
+})
+
+export const { selectPopulationCategory } = selectedPopulationCategorySlice.actions
+
+export const selectedPopulationCategoryReducer = selectedPopulationCategorySlice.reducer
