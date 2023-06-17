b598b4e31cf809240b1808d55029f3829a0f9281
yuuumiravy <hikoki.y@gmail.com>
2023-06-17 11:47:32 +0900

    feat: SelectedPopulationCategoryのStoreを削除

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import { useSelector, TypedUseSelectorHook, useDispatch } from 'react-redux'
 import { prefectureApi } from '@/features/prefectures/apis/prefectureApi'
 import { populationApi } from '@/features/populations/apis/populationApi'
-import { selectedPopulationCategoryReducer } from '@/features/populations/stores/SelectedPopulationCategory'
 
 export const store = configureStore({
   reducer: {
     [prefectureApi.reducerPath]: prefectureApi.reducer,
     [populationApi.reducerPath]: populationApi.reducer,
-    selectedPopulationCategory: selectedPopulationCategoryReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([prefectureApi.middleware, populationApi.middleware]),
